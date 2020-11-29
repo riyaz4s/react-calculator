f4c1284c0919b7a6135c3026708ce8ab8c8b439e
riyaz4s <ssr0401@gmail.com>
2020-11-29 19:36:10 +0530

    Fix percentage calculation

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,53 +1,26 @@
 import { useState } from 'react';
 import Display from './display/Display';
 import Keyboard from './keyboard/Keyboard';
-import { saveHistory } from '../../helpers/index'
+import { handleValue } from '../../helpers/index'
 import './Calculator.css'
 
 function Calculator() {
   const [text, setText] = useState('0');
   const [completed, setCompleted] = useState(false);
 
-  const handleEquals = (displayValue) => {
-    const operation = displayValue;
-    try {
-      displayValue = eval(displayValue);
-      if(isFinite(displayValue)) {
-        displayValue = displayValue.toString();
-      } else {
-        displayValue = 'Syntax error';
-      }
-    } catch(e) {
-      displayValue = 'Syntax error';
-    }
-    if(operation !== displayValue) {
-      setCompleted(true);
-      saveHistory(operation, displayValue)
-    }
-    return displayValue;
-  }
-
   const keyClick = (value) => {
     let displayValue = text;
-    const prevCompleted = completed;
+    let isNewOperation = completed;
     if(displayValue === 'Syntax error') {
       displayValue = '0'
     }
-    if(completed) {
+    if(isNewOperation) { // reset new operation flag
       setCompleted(false);
     }
-    if(value === 'equals') {
-      displayValue = handleEquals(displayValue);
-    } else if(value === 'clear') {
-      displayValue = '0';
-    } else if(value === 'backspace') {
-      displayValue = displayValue.slice(0, displayValue.length-1);
-    } else { // handles numbers
-      if(displayValue === '0' || prevCompleted) { // checks if it is a new operation
-        displayValue = value;
-      } else {
-        displayValue = `${displayValue}${value}`;
-      }
+    ({ completed: isNewOperation, displayValue } = handleValue(value, displayValue, isNewOperation));
+
+    if(completed !== isNewOperation) { // if this is a completion operation reset completed value
+      setCompleted(isNewOperation);
     }
     setText(displayValue === '' ? '0' : displayValue);
   }
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -29,7 +29,69 @@ const pushHistory = (currentHistory, value) => {
   return currentHistory;
 }
 
+const substitutePercent = (value) => {
+  let result = value.replace(/%/g, '*1/100');
+  if(value.slice(-1) === '%') {
+    result = result.slice(0, result.length-1)
+  }
+  return result;
+}
+
+const handleEquals = (displayValue) => {
+  let completed = false;
+  const operation = displayValue;
+  try {
+    displayValue = eval(substitutePercent(displayValue));
+    if(isFinite(displayValue)) {
+      displayValue = displayValue.toString();
+    } else {
+      displayValue = 'Syntax error';
+    }
+  } catch(e) {
+    displayValue = 'Syntax error';
+  }
+  if(operation !== displayValue) {
+    completed = true;
+    saveHistory(operation, displayValue)
+  }
+  return {
+    displayValue,
+    completed
+  };
+}
+
+const handleValue = (value, displayValue, isNewOperation) => {
+  let completed = isNewOperation;
+  switch(value) {
+    case 'equals':
+      ({ displayValue, completed } = handleEquals(displayValue));
+      break;
+    case '%':
+      ({ displayValue, completed } = handleEquals(`${displayValue}%`));
+      break;
+    case 'clear':
+      displayValue = '0';
+      break;
+    case 'backspace':
+      displayValue = displayValue.slice(0, displayValue.length-1);
+      break;
+    default:  // handles numbers
+      if(displayValue === '0' || isNewOperation) { // checks if it is a new operation
+        displayValue = value;
+      } else {
+        displayValue = `${displayValue}${value}`;
+      }
+      break;
+  }
+  return {
+    displayValue,
+    completed
+  }
+}
+
 export {
   saveHistory,
   getHistory,
+  substitutePercent,
+  handleValue
 }
